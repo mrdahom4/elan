2b6265baaf4846fad1b3e3934582217255e86e1e
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-01-26 12:59:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -712,21 +712,13 @@ client.on('message' , message => {
 
 
 client.on('message', message => {
-    if (message.content.startsWith("-avatar")) {
-        var mentionned = message.mentions.users.first();
-    var x5bzm;
-      if(mentionned){
-          var x5bzm = mentionned;
-      } else {
-          var x5bzm = message.author;
-          
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setImage(`${x5bzm.avatarURL}`)
-      message.channel.sendEmbed(embed);
-    }
+  
+  if (message.content === '-avatar') {
+    
+    message.reply(message.author.avatarURL);
+  }
 });
+
 
 client.on('message', message => { 
            if (message.content.startsWith(prefix + "id")) {
